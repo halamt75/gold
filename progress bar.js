40ab367c7eed3ba3476a361f879a64e5cce1dec0
Ha Lam <halamt75@yahoo.com>
2023-02-04 12:00:53 +0700

    Update progress bar.js

--- progress bar.js
+++ progress bar.js
@@ -1,21 +1,20 @@
 <script>
 per = 65; t=3000
-var w2 = parseFloat(document.querySelector('#BOX2 .ladi-box').offsetWidth);
-var c = document.querySelector('#HEADLINE1 .ladi-headline');
-function bar(id1,w2,c,per,t,w0=30){
+var width = parseFloat(document.querySelector('#BOX2 .ladi-box').offsetWidth);
+var a = document.querySelector('#HEADLINE1 .ladi-headline');
+function bar(id1,width,a,per,t,w0=30){
 	pc = 1
 	setInterval(function(){
 		if(pc<per){
 			pc +=1;
-            $(document).ready(function(){c.innerText = String(pc)+'%';});}
+            $(document).ready(function(){a.innerText = String(pc)+'%';});}
 	}, t/per-0.5);
 	wx = 0
 	setInterval(function(){
-		if(wx<(per/100*w2-w0)){
-			wx += per/100*w2+3
-            $(document).ready(function(){$("#"+id1).animate({width: String(wx)+'px'}, t);});
-		}
+		if(wx<(per/100*width-w0)){
+			wx += per/100*width+3
+            $(document).ready(function(){$("#"+id1).animate({width: String(wx)+'px'}, t);});}
 	}, 10);
 }
-bar('BOX1',w2,c,per,t)
+bar('BOX1',width,a,per,t)
 </script>
